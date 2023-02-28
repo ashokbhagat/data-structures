7249c8ebf48ef9baa816ba04a2c17e2337d21c5d
Ashok Bhagat <ashoka.bhagat@gmail.com>
2023-02-28 12:27:06 +0530

    Array reverse

---
+++ ArraysReverse.java
@@ -0,0 +1,27 @@
+public class ArraysReverse {
+
+    public static void main(String[] args) {
+        reverseArray();
+    }
+
+    private static void reverseArray() {
+        int a[] = { 1, 2, 3, 4, 5 };
+        print(a);
+
+        for (int i = 0; i < (a.length / 2); i++) {
+            int temp = a[i];
+            a[i] = a[(a.length - 1) - i];
+            a[(a.length - 1) - i] = temp;
+        }
+
+        print(a);
+
+    }
+
+    private static void print(int[] a) {
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+    }
+}
